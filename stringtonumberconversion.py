2c9a91f307e61b2d4c4c0f9dbba15b08b5c6ae36
ambikayadavannavar-sudo <ambikayadavannavar@gmail.com>
2026-01-07 23:30:30 +0530

    Add files via upload

---
+++ stringtonumberconversion.py
@@ -0,0 +1,9 @@
+price = input("Enter price of the item: ")
+quantity = input("Enter quantity: ")
+
+price = float(price)
+quantity = int(quantity)
+
+total_cost = price * quantity
+
+print("Total Cost:", total_cost)
---
+++ usinginput().py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+fav_number = input("Enter your favorite number: ")
+
+print("Hello", name, ", your favorite number is", fav_number)
+print("Data type of name:", type(name))
+print("Data type of favorite number:", type(fav_number))
